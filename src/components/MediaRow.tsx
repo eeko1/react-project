0d1b7f55877801138beac5fc255f6a26f5cc0d69
Eemi <eemi.korhonen@metropolia.fi>
2024-02-05 23:14:15 +0200

    first commit

---
+++ src/components/MediaRow.tsx
@@ -0,0 +1,26 @@
+import {Link} from 'react-router-dom';
+import {MediaItem} from '../types/DBTypes';
+
+const MediaRow = (props: {
+  item: MediaItem;
+}) => {
+  const {item}  = props;
+
+  return (
+    <tr className="media-row">
+      <td>
+        <img src={item.thumbnail} alt={item.title} />
+      </td>
+      <td>{item.title}</td>
+      <td>{item.description}</td>
+      <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+      <td>{item.filesize}</td>
+      <td>{item.media_type}</td>
+      <td>
+        <Link to="/single" state={item}>View</Link>
+      </td>
+    </tr>
+  );
+};
+
+export default MediaRow;
