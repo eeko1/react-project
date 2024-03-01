3f74889596d78111af266ea1356f4d622886a37b
Eemi <eemi.korhonen@metropolia.fi>
2024-03-01 09:41:30 +0200

    upload npm build problem fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~eemiko/hybrid-week3/',
+  base: '/~eemiko/hybrid-week4/',
 });
